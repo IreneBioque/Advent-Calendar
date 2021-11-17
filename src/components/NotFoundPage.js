e6001a7ee76dc59ef79d0cacd731265efb2414d7
IreneBioque <irenebioque@gmail.com>
2021-11-17 17:07:11 +0100

    add notfound

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import "../styles/layout/_notfoundpage.scss";
+
+const NotFoundPage = () => {
+  return (
+    <div className="notFoundPage">
+      <p className="notFoundPage__text">
+        Página no encontrada. No vayas tan rápido todavía no es el día. Tendrás que esperar un poco más
+      </p>
+      <Link to="/">
+        <p>Vuelve atrás</p>
+      </Link>
+    </div>
+  );
+};
+export default NotFoundPage;
