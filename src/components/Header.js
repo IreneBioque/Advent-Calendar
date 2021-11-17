308d12cc7d7fd96cb4e7e923e2a554e53cb19b17
IreneBioque <irenebioque@gmail.com>
2021-11-17 20:51:07 +0100

    add link to landing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,17 @@
+
 import "../styles/layout/_header.scss";
 
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <header className="header">Calendario de adviento rolero</header>;
+  return (
+    <>
+      <header className="header">Calendario de adviento rolero</header>
+      <Link to="/">
+        <p>Atrás</p>
+      </Link>
+    </>
+  );
 };
+
 export default Header;
