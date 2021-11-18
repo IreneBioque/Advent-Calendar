056708960b87b1b826ce88229ed192f1e994e7f6
IreneBioque <irenebioque@gmail.com>
2021-11-18 14:15:10 +0100

    generic component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <header className="header">
-        <h1 className="header__title">Calendario de adviento rolero</h1>
-        <Link to="/" className="header__paragraph">
+    <header className="header">
+      <Link to="/" className="header__paragraph">
         Atrás
-        </Link>
-      </header>
-    </>
+      </Link>
+      <h1 className="header__title">Calendario de adviento rolero</h1>
+      <ul className="header__list">
+        <li className="header__list--element">
+          <Link to="/participants">Participantes</Link>
+        </li>
+        <li className="header__list--element">
+          <Link to="/creators">Creadores</Link>
+        </li>
+      </ul>
+    </header>
   );
 };
 
