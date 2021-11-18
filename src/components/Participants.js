fef3aae21ccc2a56ba60d87aee99e3a8b9fbb5f6
IreneBioque <irenebioque@gmail.com>
2021-11-18 14:17:12 +0100

    use props

--- src/components/Participants.js
+++ src/components/Participants.js
@@ -1,11 +1,31 @@
 import { Link } from "react-router-dom";
 
-const Header = () => {
+import { useState } from 'react';
+import "../styles/layout/_participants.scss";
+const Participants = (props) => {
+  const list = props.datafiltered.map((participant) => {
+    return (
+      <li key={participant.id} className="participantList__element">
+        <h4>{participant.name}</h4>
+        <img src={participant.img} alt={participant.name} />
+        <p>{participant.description}</p>
+        <a href={participant.twitter} target="_blank" rel="noreferrer">
+          <i className="fab fa-twitter"></i>
+        </a>
+      </li>
+    );
+  });
   return (
     <>
-¡Holiwi! :)
+      <header className="pHeader">
+        <h1>Participantes del calendario de adviento rolero</h1>
+        <Link to="/">Atrás</Link>
+      </header>
+      <main>
+        <ul className="participantList">{list}</ul>
+      </main>
     </>
   );
 };
 
-export default Header;
+export default Participants;
