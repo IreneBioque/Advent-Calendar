739ae535b0bd73d741671a24052e9fc70dd98676
Cecrom <manu.garri@gmail.com>
2021-11-23 20:17:44 +0100

    Progreso del modal

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -3,16 +3,25 @@ import "../styles/layout/_modalwindow.scss";
 import no from "../images/NO.jpg";
 import { Modal } from 'react-bootstrap';
 
-const ModalWindow =({show, closeModal}) => {
+const ModalWindow = ({ show, closeModal, data }) => {
   return (
     <Modal show={show} onHide={closeModal} id='modal'>
-    <Modal.Header closeButton>
-      <Modal.Title>Dia 1</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      {/* Aqui poner código que nos de la gana  */}
-      Woohoo, you're reading this text in a modal!</Modal.Body>
-  </Modal>
+      <Modal.Header closeButton>
+        <Modal.Title>
+        <h1 className="modal__ModalTitle">Día {data.day}</h1>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h1 className="modal__header">Hoy tenemos un regalo de {data.autor} {`(${data.pronombres})`}</h1>
+        <div><img src={data.image} alt="Imagen del regalo"></img></div>
+        <br></br>
+        <h2 className="modal__title">Se trata de {data.title}</h2>
+        <h3 className="modal__description">{data.description}</h3>
+        <br></br>
+        <h3 className="modal__download"><button><a href={data.file}>Descarga del material</a></button></h3>
+        <h3 className="modal__rrss"><span className="fab fa-twitter"></span> {data.twitter}</h3>
+      </Modal.Body>
+    </Modal>
   );
 }
 
