b93a4a0e231b2dc2ad6accfb76e865269a3e5504
IreneBioque <irenebioque@gmail.com>
2021-11-24 11:39:27 +0100

    add basics styles landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,27 +2,27 @@ import { Link } from "react-router-dom";
 import "../styles/layout/_landing.scss";
 const Landing = () => {
   return (
-    <>
+    <div className="landing">
       {/* Lo dejo preparado para el flex */}
       <header className="landing__header">
         <h1 className="landing__header--title">¡Te damos la bienvenida al calendario de adviento rolero!</h1>
         <ul className="landing__header--list">
-          <li className="landing__header--list__element"><Link to="/form">Formulario de contacto</Link></li>
+          <li className="landing__header--list__element"><Link to="/form">Contacto</Link></li>
           <li className="landing__header--list__element"><Link to="/participants">Participantes</Link></li>
           <li className="landing__header--list__element"><Link to="/creators">Creadores</Link></li>
         </ul>
       </header>
       <main className="landing__main">
-        <div className="landing__video">
+        <div className="landing__main--video">
           <video src="">
             Su navegador no puede reproducir vídeos
           </video>
         </div>
         <Link to="/calendar">
-          <button>Ir al calendario</button>
+          <button className="landing__main--button">Ir al calendario</button>
         </Link>
       </main>
-    </>
+    </div>
   );
 };
 
