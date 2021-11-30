de7cac6f1eb8f384ce9b805d5e881aab5b7d9340
IreneBioque <irenebioque@gmail.com>
2021-11-30 11:18:37 +0100

    add  png

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,7 +9,7 @@ const Landing = () => {
       <Header/>
       <main className="landing__main">
         <div className="landing__main--video">
-        <iframe src="https://www.youtube.com/embed/hJresi7z_YM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/hJresi7z_YM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
       </main>
     </div>
--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "../styles/layout/_notfoundpage.scss";
+import notfound from "../images/NFP2.png"
 
 const NotFoundPage = () => {
   return (
     <div className="notFoundPage">
       <h1 className="notFoundPage__text">
-        Te has colao. Es decir, no sé cómo has llegado aquí, pero te aseguro que no era el camino correcto.
+        Te has colao. Es decir, no sé cómo has llegado aquí, pero te aseguro que
+        no era el camino correcto.
       </h1>
+      <img src={notfound} alt="logonotfound" className="notFoundPage__img" />
       <Link to="/">
-        <button>Volver al inicio</button>
+        <button className="notFoundPage__button">Volver al inicio</button>
       </Link>
     </div>
   );
