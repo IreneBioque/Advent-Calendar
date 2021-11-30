5f2d353027db546f09c6f8bc0f329a2fd2a214ce
IreneBioque <irenebioque@gmail.com>
2021-11-30 14:21:39 +0100

    change image

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "../styles/layout/_notfoundpage.scss";
-import notfound from "../images/NFP2.png"
-
+import error from "../images/404.png" 
 const NotFoundPage = () => {
   return (
     <div className="notFoundPage">
@@ -10,7 +9,7 @@ const NotFoundPage = () => {
         Te has colao. Es decir, no sé cómo has llegado aquí, pero te aseguro que
         no era el camino correcto.
       </h1>
-      <img src={notfound} alt="logonotfound" className="notFoundPage__img" />
+      <img src={error} alt="logonotfound" className="notFoundPage__img" />
       <Link to="/">
         <button className="notFoundPage__button">Volver al inicio</button>
       </Link>
